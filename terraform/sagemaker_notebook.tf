3ed52c895d269f94d28a25f57b8ae9b3ccc417ee
Wyatt Jones <wyattjo116@gmail.com>
2021-10-22 14:03:58 -0400

    adding sagemaker-notebook

---
+++ terraform/sagemaker_notebook.tf
@@ -0,0 +1,14 @@
+resource "aws_sagemaker_code_repository" "repo" {
+  code_repository_name = "terraform-aws-project-1"
+
+  git_config {
+    repository_url = "https://github.com/wmjones/terraform-aws-project-1"
+  }
+}
+
+resource "aws_sagemaker_notebook_instance" "ni" {
+  name                    = "my-notebook-instance"
+  role_arn                = "arn:aws:iam::761551243560:user/wyatt"
+  instance_type           = "ml.t2.medium"
+  default_code_repository = aws_sagemaker_code_repository.repo.code_repository_name
+}
